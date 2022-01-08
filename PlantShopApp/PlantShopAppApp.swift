922c39ec1d9ffeee28ef3ccfdb12ddcc86e71d64
Misha Causur <mishacausur@mail.ru>
2022-01-08 16:31:12 +0500

    Initial Commit

---
+++ PlantShopApp/PlantShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantShopAppApp.swift
+//  PlantShopApp
+//
+//  Created by Misha Causur on 08.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct PlantShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
